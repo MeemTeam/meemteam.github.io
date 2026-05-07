72191be436e6a6094387ea113c5dede58408b238
MeemTeam <meemteam75@gmail.com>
2026-05-07 19:00:56 +0300

    Add files via upload

--- login.js
+++ login.js
@@ -1,19 +1,25 @@
-function loginuser(form){
- let email=form.email.value;
- let passward=form.passward.value; 
-  if(email===""||passward===""){
-      alert("Please Fill In All Fields!");
-      return false;}
-  if(!email.includes("@")){
+function loginuser(event){
+    event.preventDefault();
+    const form = event.target;
+    let email=form.email.value;
+    let password=form.password.value; 
+    if(email===""||password===""){
+        alert("Please Fill In All Fields!");
+        return false;
+    }
+    if(!email.includes("@")){
         alert("Invalid Email");
-        return false;}
-      let users=JSON.parse(localStorage.getItem("users"))||[];
-      let user=users.find(u=>u.email===email && u.passward===passward);
+        return false;
+    }
+    let users=JSON.parse(localStorage.getItem("users"))||[];
+    let user=users.find(u=>u.email===email && u.password===password);
     if(user){
-        alert("login Successful! Welcome"+user.name);
-        window.location.href="booking.html";}
+        alert("login Successful! Welcome" + user.name);
+        window.location.href="index.html";
+    }
     else{ 
-        alert("Invalid Email Or Passward");
-        return false;}
+        alert("Invalid Email Or Password");
+        return false;
+    }
         return false;
 }
